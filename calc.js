56c0fe201a42d3ae7592eb2ef8bec862ded136b3
Liemmjo <140917436+Liemmjo24@users.noreply.github.com>
2024-01-15 13:30:02 -0500

    Update calc.js
    Added simple addition, subtraction, multiplication, division with two numbers for now.

--- calc.js
+++ calc.js
@@ -1,3 +1,15 @@
 function add(a, b){
     return a + b;
 }
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b;
+}
+console.log(add(2,3))
+console.log(multiply(2,5))
+console.log(divide(6,3))
