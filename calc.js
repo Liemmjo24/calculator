3f9a98d25faa367e10d1b92b0c51dae266cb407b
Liemmjo <140917436+Liemmjo24@users.noreply.github.com>
2024-01-19 20:47:49 -0500

    Update calc.js
    Remake the logic for the JS

--- calc.js
+++ calc.js
@@ -1,18 +1,96 @@
-function add(a, b){
-    return a + b;
+let operator ='';
+let previousValue = '';
+let currentValue = '';
+
+document.addEventListener("DOMContentLoaded", function(){
+    //Store components on HTML to JS
+
+    let clear = document.querySelector("#clear-btn");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator")
+
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+
+    //Allows the UI to know what number the users want
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+        handleNumber(e.target.textContent)
+        currentScreen.textContent = currentValue;
+    }))
+    //Allows the UI to know what operator the users want
+    operators.forEach((op) => op.addEventListener("click", function(e){
+        handleOperator(e.target.textContent)
+        previousScreen.textContent = previousValue + " " + operator;
+        currentScreen.textContent = currentValue;
+    }))
+    //Logic for Clear
+    clear.addEventListener("click", function(){
+        previousValue = '';
+        currentValue = '';
+        operator = '';
+        previousScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;
+    })
+    //Logic for computation
+    equal.addEventListener("click", function(){
+        if (currentValue != '' && previousValue != ''){
+            calculate();
+            previousScreen.textContent = '';
+            if(previousValue.length <= 5){
+                currentScreen.textContent = previousValue;
+            } else {
+                currentScreen.textContent = previousValue.slice(0,5) + "...";
+            }
+        }
+    })
+    decimal.addEventListener("click", function(){
+        addDecimal()
+    })
+})
+
+//Calculator shows only 5 numbers
+function handleNumber(num){
+    if(currentValue.length <= 5){
+        currentValue += num;
+    }
 }
-function subtract(a,b){
-    return a - b;
+
+//Logic for operator
+function handleOperator(op){
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
 }
-function multiply(a,b){
-    return a * b;
+//Logic for computation
+function calculate(){
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if(operator === "+"){
+        previousValue += currentValue;
+    } else if (operator === "-"){
+        previousValue -= currentValue;
+    } else if (operator === "*"){
+        previousValue *= currentValue;
+    } else if (operator === "/"){
+        previousValue /= currentValue;
+    }
+    previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
+    console.log(previousValue)
 }
-function divide(a,b){
-    return a / b;
+
+//Calculator to round up
+function roundNumber(num){
+    return Math.round(num * 1000) / 1000;
 }
-function operate(operator, a, b){
-    
+
+//Shows decimals if computation has one
+function addDecimal(){
+    if(!currentValue.includes(".")){
+        currentValue += '.';
+    }
 }
-console.log(add(2,3))
-console.log(multiply(2,5))
-console.log(divide(6,3))
